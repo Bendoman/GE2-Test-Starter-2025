c7f3757f9b082e8977ad92f23f15aeea364fc0b9
Ben Corran <ben.corran@sap.com>
2025-04-29 10:10:35 +0100

    Added forward and backward movement plus drag

--- drone_controller.gd
+++ drone_controller.gd
@@ -1,14 +1,18 @@
 extends CharacterBody3D
 
 var gravity:Vector3 = Vector3(0, -9.8, 0)
+var drag:Vector3 = Vector3(0, 0, 2)
 
 @export var liftIntensity = 25
 var liftForce:Vector3 = Vector3(0, liftIntensity, 0)
 
+@export var driveIntensity = 15
+var driveForce:Vector3 = Vector3(0, 0, driveIntensity)
+
 func _ready():
 	pass
 	
-func _process(delta):
+func _process(delta): 
 	
 	pass
 
@@ -17,4 +21,18 @@ func _physics_process(delta):
 	
 	if(Input.is_action_pressed("up")):
 		velocity += (liftForce * delta)
+		
+	if(global_position.y > 1):
+		var move = Input.get_axis("forward", "reverse")
+		if(move != 0):
+			velocity += (driveForce * delta * move)
+			
+		
+
+	# Apply drag 
+	if(velocity.z < 0):
+		velocity += (delta * drag)
+	elif(velocity.z > 0):
+		velocity += (delta * -drag)
+	
 	move_and_slide()
