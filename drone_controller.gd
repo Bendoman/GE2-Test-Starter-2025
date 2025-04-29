5339b59368d1ef0c5e4c3942bfac7ac16f25a5c3
Ben Corran <ben.corran@sap.com>
2025-04-29 09:41:19 +0100

    Added gravity to drone

--- drone_controller.gd
+++ drone_controller.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+var gravity:Vector3 = Vector3(0, -9.8, 0)
+
 func _ready():
 	pass
 	
@@ -8,5 +10,10 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
+	velocity += gravity * delta
 	
+	
+	
+	
+	move_and_slide()
 	pass
