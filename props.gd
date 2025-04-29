173e7b06aa8cb88835462e202a9d6f8061782c7a
Ben Corran <ben.corran@sap.com>
2025-04-29 10:00:36 +0100

    Added rotor spinning

--- props.gd
+++ props.gd
@@ -1,12 +1,15 @@
 extends Node3D
 
-@export var MAX_SPEED = 10
+@export var MAX_SPEED = 15
 var currentSpeed = 0
 
 func _ready():
 	pass
 
 func _process(delta): 
-
+	if(Input.is_action_pressed("up")):
+		currentSpeed += .05
+	elif(currentSpeed > 0): 
+		currentSpeed -= .05
 
 	rotate_y(currentSpeed)
