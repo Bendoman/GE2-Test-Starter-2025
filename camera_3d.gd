67a163a5db249554d9d2116934708cd8bd36b411
Ben Corran <ben.corran@sap.com>
2025-04-29 09:57:06 +0100

    Added camera lerping

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,1 +1,16 @@
 extends Camera3D
+
+var target
+var cam
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	target = get_parent().find_child("CameraMarker", true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	# transform.origin += transform.basis.z
+	global_position = lerp(global_position, target.global_position, delta*7)
+	look_at(target.get_parent_node_3d().global_position, Vector3.UP)
--- props.gd
+++ props.gd
@@ -1,14 +1,12 @@
 extends Node3D
 
 @export var MAX_SPEED = 10
-var currentRotation = 0
+var currentSpeed = 0
 
 func _ready():
 	pass
 
 func _process(delta): 
 
-		
-		
-		
-	pass
+
+	rotate_y(currentSpeed)
