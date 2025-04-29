b04e89d710ead70526666a0ed00ad31daaef0ae4
Ben Corran <ben.corran@sap.com>
2025-04-29 09:46:17 +0100

    Added lift force to drone

--- drone_controller.gd
+++ drone_controller.gd
@@ -2,6 +2,9 @@ extends CharacterBody3D
 
 var gravity:Vector3 = Vector3(0, -9.8, 0)
 
+@export var liftIntensity = 25
+var liftForce:Vector3 = Vector3(0, liftIntensity, 0)
+
 func _ready():
 	pass
 	
@@ -10,10 +13,8 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	velocity += gravity * delta
+	velocity += (gravity * delta)
 	
-	
-	
-	
+	if(Input.is_action_pressed("up")):
+		velocity += (liftForce * delta)
 	move_and_slide()
-	pass
